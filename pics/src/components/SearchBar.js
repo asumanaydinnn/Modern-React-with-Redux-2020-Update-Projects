140c9d49729c329aa9f09680193456e395bd185e
Asuman Aydın <asuman.aydin6709@gmail.com>
2021-09-08 15:40:12 +0300

    new commit to main, pics

---
+++ pics/src/components/SearchBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <input type="text"></input>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
