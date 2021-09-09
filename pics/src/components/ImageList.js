b0c9c63afebf9b17a5873cf97b2dd67e9806ae9e
Asuman Aydın <asuman.aydin6709@gmail.com>
2021-09-09 19:38:54 +0300

    api call for images to search bar

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,8 +1,19 @@
 import React, { Component } from "react";
+import "./ImageList.css";
 
 class ImageList extends Component {
   render() {
-    return <div>Found: {this.props.imageList.length} images</div>;
+    return (
+      <div className="ui segment">
+        {this.props.imageList.map((image) => {
+          return (
+            <div className="image-list" key={image.id}>
+              <img alt={image.description} src={image.urls.regular} />
+            </div>
+          );
+        })}
+      </div>
+    );
   }
 }
 
