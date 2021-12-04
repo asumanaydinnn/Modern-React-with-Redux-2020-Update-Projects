3420512a56c2687960ea92cffeb473e247b7c36e
Asumanaydin <asuman.aydin6709@gmail.com>
2021-12-04 12:49:50 +0300

    translate app

---
+++ translate/src/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
+import Translate from "./components/Translate";
+
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a front end javascript framework",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite JS library among engineers",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating components",
+  },
+];
+
+const options = [
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "The Color Green",
+    value: "green",
+  },
+  {
+    label: "A Shade of Blue",
+    value: "blue",
+  },
+];
+
+const App = () => {
+  return (
+    <div>
+      <Translate />
+    </div>
+  );
+};
+export default App;
